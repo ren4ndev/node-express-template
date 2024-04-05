4ca8ec6b2bde9a29b6fe6849e1a1c0f42c148180
Renan Oliveira <oliveira.renan.rg@gmail.com>
2024-04-05 10:43:41 -0300

    chore: Setup jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+module.exports = {
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+  },
+};
